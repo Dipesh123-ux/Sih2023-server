2420d8d051bada890260a5956b37863ebc56deff
dipesh <jaswanidipesh8@gmail.com>
2023-09-20 17:00:53 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,11 @@
-user.select('-password')
+const express = require("express");
+const router = express.Router();
+const {
+    registerPatient,
+    loginPatient,
+} = require("../controllers/auth");
+
+router.post('/registerpatient',registerPatient);
+router.post('/loginpatient',loginPatient);
+
+module.exports = router;
